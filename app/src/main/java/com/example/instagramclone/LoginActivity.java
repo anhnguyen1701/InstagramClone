76f274406d901bf80b56cfe199c887a41258b4c0
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-13 22:33:02 +0700

    user authentication

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -7,11 +7,16 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,44 +44,55 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         txt_signup = findViewById(R.id.txt_signup);
 
+        auth = FirebaseAuth.getInstance();
+
         txt_signup.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
 
-        login.setOnClickListener(view -> {
-            ProgressDialog pd = new ProgressDialog(LoginActivity.this);
-            pd.setMessage("Please wait..");
-            pd.show();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+                pd.setMessage("Please wait...");
+                pd.show();
 
-            String str_email = email.getText().toString();
-            String str_password = password.getText().toString();
+                String str_email = email.getText().toString();
+                String str_password = password.getText().toString();
 
-            if (TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)) {
-                Toast.makeText(LoginActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
-            } else {
-                auth.signInWithEmailAndPassword(str_email, str_password).addOnCompleteListener(LoginActivity.this, task -> {
-                    if (task.isSuccessful()) {
-                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(Objects.requireNonNull(auth.getCurrentUser()).getUid());
-                        reference.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                pd.dismiss();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
-                                finish();
-                            }
+                if (TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)) {
+                    Toast.makeText(LoginActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(str_email, str_password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
 
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-                                pd.dismiss();
-                            }
-                        });
-                    } else {
-                        pd.dismiss();
-                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users")
+                                                .child(auth.getCurrentUser().getUid());
+
+                                        reference.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                pd.dismiss();
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                pd.dismiss();
+                                            }
+                                        });
+                                    } else {
+                                        pd.dismiss();
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
             }
         });
-
     }
 }
